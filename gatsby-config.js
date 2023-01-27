8ae955c16cf39a92068dd0b63298ddf511274d34
Bob_Smith <bobsmith@eab.com>
2023-01-27 00:24:52 -0500

    save work in progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `Gatsby Serverless Functions`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ['gatsby-plugin-netlify'],
+};
---
+++ src/api/boop.js
@@ -0,0 +1,53 @@
+import Cors from 'cors';
+
+const allowedOrigins = [
+  'https://paulieapi.gatsbyjs.io',
+  'https://www.mdx-embed.com',
+  'https://paulie.dev',
+];
+
+const cors = Cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error());
+    }
+  },
+});
+
+const runCorsMiddleware = (req, res) => {
+  return new Promise((resolve, reject) => {
+    cors(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+};
+
+export default async function handler(req, res) {
+  const { url } = req.body;
+  console.log("🚀 ~ file: boop.js:32 ~ handler ~ url", url)
+  console.log("🚀 ~ file: boop.js:32 ~ handler ~ req.body", req.body)
+
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      await runCorsMiddleware(req, res);
+    }
+    try {
+      if (!url) {
+        res.status(400).json({ message: '⚠️ Missing required body params' });
+      }
+
+      res
+        .status(200)
+        .json({ message: '🕺 Retreiving page HTML ok', url: session.url });
+    } catch (error) {
+      res.status(500).json({ message: '🚫 Retrieving HTML error' });
+    }
+  } catch (error) {
+    res.status(403).json({ message: '🚫 Request blocked by CORS' });
+  }
+}
